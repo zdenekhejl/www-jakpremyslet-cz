245cc270f894d21d55d38cbd53d69bb369408e46
zdenekhejl <zdenek@zdenek-hejl.com>
2021-01-03 12:00:05 +0100

    favicons passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -178,6 +178,14 @@ module.exports = function(eleventyConfig) {
 
   // Don't process folders with static assets e.g. images
   eleventyConfig.addPassthroughCopy("favicon.ico");
+  eleventyConfig.addPassthroughCopy("android-chrome-192x192.png");
+  eleventyConfig.addPassthroughCopy("android-chrome-512x512.png");
+  eleventyConfig.addPassthroughCopy("apple-touch-icon.png");
+  eleventyConfig.addPassthroughCopy("favicon-16x16.png");
+  eleventyConfig.addPassthroughCopy("favicon-32x32.png");
+  eleventyConfig.addPassthroughCopy("site.webmanifest");
+
+
   eleventyConfig.addPassthroughCopy("images/")
   eleventyConfig.addPassthroughCopy("content/images/")
   eleventyConfig.addPassthroughCopy("admin");
