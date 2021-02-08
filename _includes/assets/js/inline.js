a6c7f34ea61f821b3b283205e0df0784b9aa4215
zdenekhejl <zdenek@zdenek-hejl.com>
2021-02-08 23:30:05 +0100

    orpava mobilniho menu

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -28,13 +28,13 @@ function logout() {
 function showNavigation() {
   const navigation = document.getElementById("navigation");
   navigation.classList.remove("hidden", "sticky","pt-32"); 
-  navigation.classList.add("absolute","right-0", "top-0", "-mt-0", "z-50", "pt-0", "bg-white","border-l", "border-gray-200"); 
+  navigation.classList.add("right-0", "top-0", "-mt-0", "z-50", "pt-0", "bg-white","border-l", "border-gray-200"); 
 }
 
 function closeNavigation() {
   const navigation = document.getElementById("navigation");
   navigation.classList.add("hidden"); 
-  navigation.classList.remove("absolute","right-0","z-50", "bg-gray-100", "border-r", "border-gray-800" ); 
+  navigation.classList.remove("right-0","z-50", "bg-gray-100", "border-r", "border-gray-800" ); 
 }
 
 function activateDarkMode() {
